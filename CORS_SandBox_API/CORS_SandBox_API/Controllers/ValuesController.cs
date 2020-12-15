cdbbd4e89fb992c93c24006680651344dae979ab
chuangmaster <masterchuang@urtody.com>
2020-12-15 11:16:40 +0800

    refactor Simple and Preflight Request demo part

--- CORS_SandBox_API/CORS_SandBox_API/Controllers/ValuesController.cs
+++ CORS_SandBox_API/CORS_SandBox_API/Controllers/ValuesController.cs
@@ -11,7 +11,7 @@ namespace CORS_SandBox_API.Controllers
 {
     public class ValuesController : ApiController
     {
-
+        #region Simple Request Demo
         [HttpPost]
         [Route("~/api/v1/simpleRequest")]
         public HttpResponseMessage SimpleRequest([FromBody]string value)
@@ -21,7 +21,10 @@ namespace CORS_SandBox_API.Controllers
             resp.Content.Headers.Add("Access-Control-Allow-Origin", "*");
             return resp;
         }
+        #endregion
 
+
+        #region Preflight Request Demo
         [HttpPost]
         [Route("~/api/v1/normalRequest/error")]
         public HttpResponseMessage NormalRequest_error(NormalRequestParameter parameter)
@@ -42,11 +45,16 @@ namespace CORS_SandBox_API.Controllers
 
             //if mark following, you will get "Response to preflight request doesn't pass access control
             //check: No 'Access-Control-Allow-Origin' header is present on the requested resource" in actual request
-            //resp.Content.Headers.Add("Access-Control-Allow-Origin", "*");
+            resp.Content.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            //if mark following, you will get "Response to preflight request doesn't pass access control check: No
+            //'Access-Control-Allow-Origin' header is present on the requested resource."
+            //resp.Content.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
             return resp;
         }
+        #endregion
 
-
+        
         [HttpPost]
         //[CorsHandle]
         //[EnableCors("https://localhost:44354", "*", "POST")]
@@ -68,9 +76,6 @@ namespace CORS_SandBox_API.Controllers
             resp.Content.Headers.Add("Access-Control-Allow-Origin", "*");
 
             //resp.Content.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
-
-
-
             return resp;
         }
 
